d3b7bc3f9e2bef1bc6254f5abb3958489fda74b5
AgosBeggi <you@example.com>
2021-07-05 02:37:39 -0300

    ejercicio dos, primera parte

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,18 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	/*para definir variables en js
+	antes se ponía "var" y el nombre
+	de la referencia de lo que va a 
+	contener la variable*/
+	let nombre; //hasta el 2015 en js se definían con "var", ahora es "let"
+	/*también se puede inicializar escribiendo
+	let nombre="Juan";*/
+	nombre=prompt("Escribir un nombre");
+	/*también se puede escribir así
+	*/
+	alert(nombre);
+	//un espacio en blanco es esto: " "
+	//una cadena vacía es esto: ""
 }
 
