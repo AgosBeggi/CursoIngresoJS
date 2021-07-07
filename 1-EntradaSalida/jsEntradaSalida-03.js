58ac3e0753754acbf157ca0c7e714e701c0e72f4
AgosBeggi <you@example.com>
2021-07-07 13:43:29 -0300

    ejercicio de cuadro de texto html 2

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -5,12 +5,21 @@ function mostrar()
 {
 	/*var nombreIngresado;
 	nombreIngresado=txtIdNombre.value;
+	el txtIdNombre es una variable que crea el navegador con el mismo
+	nombre que le pone el programador al htmal, no todos los navegadores
+	hacen esto, porque lo que si queremos que funcione en cualquier
+	navegador, es mejor hacerlo con los comandos de js como aparece abajo
+	chrome lo hace y firefox lo incorporó en 2019
 	alert("ok");
 	esto se puede usar, pero no es 
 	js puro*/
 	let nombre;
 	nombre=document.getElementById("txtIdNombre").value;
 	alert(nombre);
+	document.getElementById("txtIdNombre").value="";
+	/*esto se hace para que la caja de texto quede vacía
+	despues de mostrar la alerta, pero no borra la variable
+	nombre, eso ya queda guardado*/
 
 }
 
