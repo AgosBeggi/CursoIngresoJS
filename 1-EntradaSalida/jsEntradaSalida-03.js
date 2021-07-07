b2a40c0d788357d8b2307cab80e237c3b4946a71
AgosBeggi <you@example.com>
2021-07-07 13:24:35 -0300

    prueba de caja de texto en htmal

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,9 +3,14 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
+	/*var nombreIngresado;
 	nombreIngresado=txtIdNombre.value;
 	alert("ok");
+	esto se puede usar, pero no es 
+	js puro*/
+	let nombre;
+	nombre=document.getElementById("txtIdNombre").value;
+	alert(nombre);
 
 }
 
