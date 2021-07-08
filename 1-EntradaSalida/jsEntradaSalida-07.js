3ef81bec3a6b49ada17b9b1ce39aad3079bc8a9d
AgosBeggi <you@example.com>
2021-07-08 20:46:47 -0300

    ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,56 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	//txtIdNumeroUno
+	//txtIdNumeroDos
+	let num1;
+	let num2;
+	let resultado;
+	num1=document.getElementById("txtIdNumeroUno").value;
+	num2=document.getElementById("txtIdNumeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resultado=num1+num2;
+	alert(resultado);	
 }
 
 function restar()
 {
-	
+	let num1;
+	let num2;
+	let resultado;
+	num1=document.getElementById("txtIdNumeroUno").value;
+	num2=document.getElementById("txtIdNumeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resultado=num1-num2;
+	alert(resultado);	
 }
 
 function multiplicar()
 { 
-	
+	let num1;
+	let num2;
+	let resultado;
+	num1=document.getElementById("txtIdNumeroUno").value;
+	num2=document.getElementById("txtIdNumeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resultado=num1*num2;
+	alert(resultado);	
 }
 
 function dividir()
 {
-	
+	let num1;
+	let num2;
+	let resultado;
+	num1=document.getElementById("txtIdNumeroUno").value;
+	num2=document.getElementById("txtIdNumeroDos").value;
+	num1=parseInt(num1);
+	num2=parseInt(num2);
+	resultado=num1/num2;
+	resultado=parseFloat(resultado);
+	alert(resultado);	
 }
 
