905ca30a4d09dbbed29f1bc96546ff20675413a7
AgosBeggi <you@example.com>
2021-07-08 22:24:52 -0300

    guardado del resto de los ejercicios E/S

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,11 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
-
-	alert("ok");
+	let num1;
+	let porcentaje;
+	let resultado;
+	num1=parseInt(document.getElementById("txtIdSueldo").value);
+	porcentaje=num1*10/100;
+	resultado=num1+porcentaje;
+	document.getElementById("txtIdResultado").value=resultado;	
 }
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,5 +5,11 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	let num1;
+	let porcentaje;
+	let resultado;
+	num1=parseInt(document.getElementById("txtIdImporte").value);
+	porcentaje=num1*25/100;
+	resultado=num1-porcentaje;
+	document.getElementById("txtIdResultado").value=resultado;	
 }
